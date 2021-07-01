c0b073259845409e15af855279386f5bd946c906
danielm <play.daniel@hotmail.com>
2021-06-30 22:26:19 -0300

    23-03-2021 [ 4.1 | JavaScript - Primeiros passos]

---
+++ script.js
@@ -0,0 +1,11 @@
+let idade = 55;
+
+if (idade > 0 && idade <= 15) {
+	console.log("Criança");
+}else if (idade > 15 && idade <= 19) {
+	console.log("Adolescência");
+}else if (idade > 19 && idade <= 50) {
+	console.log("adulto");
+}else if (idade > 51) {
+	console.log("terceira idade");
+}
