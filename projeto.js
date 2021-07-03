67bb70950b577995840132c3cfa5ef44f74f8d37
danielm <play.daniel@hotmail.com>
2021-07-02 23:42:40 -0300

    [Projeto] - [200~1 - Crie uma função usando o operador &&

---
+++ projeto.js
@@ -0,0 +1,11 @@
+// ola =true;
+// ola2 = true;
+// function compareTrue(comp1, comp2) {
+// 	if((comp1 === true)&& (comp2===true)){
+// 		console.log("true");
+// 	}else{
+// 		console.log("false");
+// 	}
+//   }
+
+
