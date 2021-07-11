5a40daf38ea09628ff9facc51692464e97a2de5d
danielm <play.daniel@hotmail.com>
2021-07-10 23:12:28 -0300

    date change

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ if (idade > 0 && idade <= 15) {
 	console.log("Criança");
 }else if (idade > 15 && idade <= 19) {
 	console.log("Adolescência");
-}else if (idade > 19 && idade <= 50) {
+}else if (idade > 29 && idade <= 50) {
 	console.log("adulto");
 }else if (idade > 51) {
 	console.log("terceira idade");
